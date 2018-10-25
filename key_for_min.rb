4b60b6ba26e632aa652eadeda5c6e66daa073f3a
Robin Kim <rkim1214@gmail.com>
2018-10-25 02:22:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,10 +18,12 @@ def key_for_min_value(name_hash)
     end 
   end 
   
+  lowest_value_key = ""
   name_hash.each do |key, value|
     if value == lowest_value
-      key 
+      lowest_value_key = key 
     end 
   end 
+  lowest_value_key
 end
 
