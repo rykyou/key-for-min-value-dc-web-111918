d2d10e3c99b7e1b1778b70b63e02ec7436fcf1f3
Robin Kim <rkim1214@gmail.com>
2018-10-25 02:26:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,23 +11,24 @@ def key_for_min_value(name_hash)
     end 
   
     count = 0 
-  lowest_value = ""
-  value_array.each do |current_value|
-    if current_value <= value_array[0]
-      lowest_value = current_value
-      count += 1
-    else
-      count += 1 
+    lowest_value = ""
+    value_array.each do |current_value|
+      if current_value <= value_array[0]
+        lowest_value = current_value
+        count += 1
+      else
+        count += 1 
+      end 
     end 
-  end 
   
-  lowest_value_key = ""
-  name_hash.each do |key, value|
-    if value == lowest_value
-      lowest_value_key = key 
+    lowest_value_key = ""
+    name_hash.each do |key, value|
+      if value == lowest_value
+        lowest_value_key = key 
+      end 
     end 
+    
+    lowest_value_key
   end 
-  lowest_value_key
-end 
 end
 
